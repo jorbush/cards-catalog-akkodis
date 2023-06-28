fc6976a43198f9ceb321ebb79d72bf618375fc67
Jordi <55429631+jorbush@users.noreply.github.com>
2023-06-28 06:49:32 +0200

    Styling form

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import 'tailwindcss/tailwind.css';
 import User from '../../types/User';
+import { IoMdClose } from "react-icons/io";
+
 
 
 interface UserFormProps {
@@ -38,42 +40,87 @@ const UserForm: React.FC<UserFormProps> = ({
     }
   
     return (
-        <div>
+        <div className="
+            justify-center 
+            items-center 
+            flex 
+            overflow-x-hidden 
+            overflow-y-auto 
+            fixed 
+            inset-0 
+            z-50 
+            outline-none 
+            focus:outline-none
+            bg-neutral-800/70
+        ">
+        <div className="
+            relative 
+            w-full
+            sm:w-5/6
+            md:w-4/6
+            lg:w-3/6
+            xl:w-2/5
+            my-6
+            mx-auto 
+            h-auto 
+            bg-white
+            rounded-xl
+            p-6
+          "
+        >
+            <div className='
+                flex
+                flex-row
+            '>
+                <IoMdClose size={18} onClick={onToggleForm}/>
+
+            </div>
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input
-                        type="text"
-                        id="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
+                <div className='
+                    flex
+                    flex-col
+                    items-center
+                    pt-6
+                    gap-5
+                '>
+                    <div>
+                        <label htmlFor="name">Name:</label>
+                        <input
+                            type="text"
+                            id="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="favoriteCars">Favorite cars:</label>
+                        <input
+                            type="text"
+                            id="favoriteCars"
+                            value={favoriteCars.join(',')}
+                            onChange={(e) =>
+                                setFavoriteCars(
+                                e.target.value.split(',').map((car) => parseInt(car.trim(), 10))
+                                )
+                            }
+                        />
+                    </div>
+                    <button type="submit">Add User</button>
                 </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="favoriteCars">Favorite cars:</label>
-                    <input
-                        type="text"
-                        id="favoriteCars"
-                        value={favoriteCars.join(',')}
-                        onChange={(e) =>
-                            setFavoriteCars(
-                              e.target.value.split(',').map((car) => parseInt(car.trim(), 10))
-                            )
-                        }
-                    />
-                </div>
-                <button type="submit">Add User</button>
+                
             </form>
         </div>
+        </div>
+
     );
   };
   
