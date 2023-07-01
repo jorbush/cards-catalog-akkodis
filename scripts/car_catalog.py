9ca931d17d86a133ab2f8ecdc478a96d89a73d5f
Jordi <55429631+jorbush@users.noreply.github.com>
2023-07-01 11:36:28 +0200

    Adding functions

--- scripts/car_catalog.py
+++ scripts/car_catalog.py
@@ -2,18 +2,25 @@ import argparse
 
 
 def car_catalog():
-
     parser = argparse.ArgumentParser(description='Car catalog')
 
-    parser.add_argument('-a', '--add', type=str, help='Add a new user')
-    parser.add_argument('-d', '--delete', type=str, help='Delete user with ID')
+    parser.add_argument('-a', '--add', action='store_true', help='Add a new user')
+    parser.add_argument('-d', '--delete', type=str, metavar='ID', help='Delete user with ID')
 
     args = parser.parse_args()
 
     if args.add:
-        print('Add:', args.add)
+        add_new_user()
     if args.delete:
-        print('Delete:', args.delete)
+        delete_user(args.delete)
+
+
+def add_new_user():
+    print('You have selected add a new use.')
+
+
+def delete_user(user_id: str):
+    print('Deleting the user with the ID ' + user_id + "...")
 
 
 if __name__ == '__main__':
