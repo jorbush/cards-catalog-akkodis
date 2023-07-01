a16d02505c838274cd9873534a67069fad8484ca
Jordi <55429631+jorbush@users.noreply.github.com>
2023-07-01 11:50:04 +0200

    Requesting user data to add the new user

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 https://cars-catalog-akkodis.netlify.app/
 
+- Python files: /scripts
+    Install requirements
+    
+        pip install -r requirements.txt
+    
--- scripts/car_catalog.py
+++ scripts/car_catalog.py
@@ -1,4 +1,6 @@
 import argparse
+import re
+from termcolor import cprint
 
 
 def car_catalog():
@@ -16,7 +18,21 @@ def car_catalog():
 
 
 def add_new_user():
-    print('You have selected add a new use.')
+    print('You have selected to add a new user.')
+    username = input("Enter the username (maximum 10 letters): ")
+    email = input("Enter the email address: ")
+
+    while not re.match(r'^.{0,10}$', username):
+        cprint("Error: The username should not exceed 10 letters.", "red")
+        username = input("Enter the username (maximum 10 letters): ")
+
+    while not re.match(r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$', email):
+        cprint("Error: Invalid email address format.", "red")
+        email = input("Enter the email address: ")
+
+    cprint("New user added successfully!", "green")
+    print("Username:", username)
+    print("Email:", email)
 
 
 def delete_user(user_id: str):
