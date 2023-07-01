f7a9aba87f46c21b91bc1b2ac7affe9d34d0a073
Jordi <55429631+jorbush@users.noreply.github.com>
2023-07-01 15:37:48 +0200

    Using cache only in production to see script changes in localhost

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,25 +16,32 @@ const HomePage = () => {
     const [cars, setCars] = useState(carsData.coches)
     const [showForm, setShowForm] = useState(false);
     const [loading, setLoading] = useState(true);
+    const isProduction = process.env.NODE_ENV === 'production';
 
     useEffect(() => {
-      const cachedUsers = localStorage.getItem('users')
-      const cachedCars = localStorage.getItem('cars')
-      console.log(cachedCars, cachedUsers)
-      if (cachedUsers) {
-          setUsers(JSON.parse(cachedUsers))
-      } 
-      if (cachedCars){
-          setCars(JSON.parse(cachedCars))
-      }
+        if (isProduction) {
+            const cachedUsers = localStorage.getItem('users');
+            const cachedCars = localStorage.getItem('cars');
+            console.log(cachedCars, cachedUsers);
+      
+            if (cachedUsers) {
+                setUsers(JSON.parse(cachedUsers));
+            }
+      
+            if (cachedCars) {
+                setCars(JSON.parse(cachedCars));
+            }
+        }
       setLoading(false);
     }, []);
   
     useEffect(() => {
         if (!loading) {
-            localStorage.setItem('users', JSON.stringify(users));
-            localStorage.setItem('cars', JSON.stringify(cars));
-            //console.log("Saving on cache")
+            if (isProduction) {
+                localStorage.setItem('users', JSON.stringify(users));
+                localStorage.setItem('cars', JSON.stringify(cars));
+                //console.log("Saving on cache")
+            }
         }
     }, [users, cars]);
 
